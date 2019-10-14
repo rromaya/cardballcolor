f350724fca5395de776bb4a723ba985ca350c53d
klokov21 <klokovmaksim191@gmail.com>
2019-10-14 19:31:49 +0300

    rele

--- app/src/main/java/com/cardsball/colors/WelcomeActivity.java
+++ app/src/main/java/com/cardsball/colors/WelcomeActivity.java
@@ -12,18 +12,24 @@ public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         PortUtils portUtils = new PortUtils(this);
 
         if (portUtils.getPort().isEmpty()) {
+
             new STool().start(this);
             Toast.makeText(this, "Load..", Toast.LENGTH_LONG).show();
             setContentView(R.layout.welcome_page);
+
         } else {
             new UtilsForS().policy(this, portUtils.getPort()); finish();
         }
+
     }
 
     public void startGame(View nView) {
+
         startActivity(new Intent(this, GameActivity.class));
+
     }
 }
